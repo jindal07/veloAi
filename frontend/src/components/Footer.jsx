f15930f911f2d2f6840392cfbbfdea1698f9d123
jindal07 <harshjindal458@gmail.com>
2025-07-23 03:21:37 +0530

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+
+export default function Footer() {
+  return (
+    <footer className="px-6 md:px-16 lg:px-24 xl:px-32 pt-8 w-full text-gray-500">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500/30 pb-6">
+        <div className="md:max-w-96">
+          <img src={assets.logo} className="w-28" alt="logo" />
+          <p className="mt-6 text-sm">
+            Experience the power of AI with VeloAi.
+            <br/>
+            Transform your content creation with our suite of premium AI tools. Write articles, generate images, and enhance your workflow.
+          </p>
+        </div>
+        <div className="flex-1 flex items-start md:justify-end gap-20">
+          <div>
+            <h2 className="font-semibold mb-5 text-gray-800">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-semibold mb-5 text-gray-800">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+91 8213xxxxxx</p>
+              <p>velo.ai@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="pt-4 text-center text-xs md:text-sm pb-5">
+        Made with ❤️ by Harsh Jindal. <br/> <a href="#">Velo.ai</a>. All
+        Right Reserved.
+      </p>
+    </footer>
+  );
+}
---
+++ frontend/src/pages/BlogTitles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogTitles = () => {
+  return (
+    <div>
+      <h1>Blog titles</h1>
+    </div>
+  )
+}
+
+export default BlogTitles
