04c16d3e9bba074d24507dd81a57bc697cdbe3d7
jindal07 <harshjindal458@gmail.com>
2025-08-12 23:26:31 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -38,6 +38,5 @@ app.use('/api/user',userRouter)
 const port =process.env.PORT || 3000;
 
 
-if (process.env.NODE_ENV !== 'production') {
+
   app.listen(port, () => console.log('Server started on port : ' + port));
-}
